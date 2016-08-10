2b51e3e8cd3f14b6b20aa0dc9ab37b9c503379ba
Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
2016-08-10 10:12:39 +1000

    added packages stub

---
+++ packages.el
@@ -0,0 +1,15 @@
+;;; packages.el --- geben layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst geben-packages
+  '(
+    geben
+    )
